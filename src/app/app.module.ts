c9897fa2e60ff1ec12e1479dd44263e7b1b444c9
www-net <78052177+www-net@users.noreply.github.com>
2023-09-25 10:45:02 +0300

    first commit. feat: add angular-material, login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from "./material-design/material-design.module";
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
